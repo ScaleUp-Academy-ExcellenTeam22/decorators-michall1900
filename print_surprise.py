59e45f43f99687fa15a4b7990fb44205cf25216a
Michal Bucks <michall1900@gmail.com>
2022-05-01 15:13:28 +0300

    Edit documentation.

--- print_surprise.py
+++ print_surprise.py
@@ -6,24 +6,28 @@ from typing import Any
 def surprise_decorator(_function: Callable) -> Callable:
     """
     Receives function and arguments and print surprise instead of the functionality of the function.
+
     :param _function: Any kind of function.
     :return: The wrapper.
     """
     @wraps(_function)
     def wrapper(*_args: Any, **_kwargs: Any) -> None:
-        """Prints surprise."""
         print("Surprise!")
     return wrapper
 
 
 @surprise_decorator
 def times2(number: float) -> float:
-    """Returns number * 2."""
+    """
+    Receives a number and return number * 2.
+    
+    :param number : A number to calculate number * 2.
+    :return: The result of number * 2.
+    """
     return number * 2
 
 
 def main_surprise() -> None:
-    """Test surprise decorator."""
     times2(5.7)
     length = surprise_decorator(len)
     length("hello", hey=123)
